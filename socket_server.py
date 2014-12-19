5cdf99fcdda602b5dc81a498d6cdb2f21825b2a6
pyoung <paul.andy.young@gmail.com>
2014-12-19 15:39:26 -0800

    reworked the code a bit to make it more modular

--- socket_server.py
+++ socket_server.py
@@ -1,12 +1,26 @@
 """
 Simple socket server for testing
+default port 1313, use -p to change
 """
 
 import SocketServer
 import json
+import sys, getopt
+
+
+def get_args(argv):
+    """ get sys args """
+    port = 1313
+    opts, args = getopt.getopt(argv[1:], "p:", ["port="])
+    for opt, arg in opts:
+        if opt in ('-p', '--port'):
+            port = arg
+    return port
+
 
 class MyTCPServer(SocketServer.ThreadingTCPServer):
     allow_reuse_address = True
+
 
 class MyTCPServerHandler(SocketServer.BaseRequestHandler):
     def handle(self):
@@ -17,7 +31,16 @@ class MyTCPServerHandler(SocketServer.BaseRequestHandler):
             # send some 'ok' back
             self.request.sendall(json.dumps({'return':'ok'}))
         except Exception, e:
-            print "Exception wile receiving message: ", e
+            print "Exception while receiving message: ", e
 
-server = MyTCPServer(('0.0.0.0', 1313), MyTCPServerHandler)
-server.serve_forever()
+
+def main(argv):
+    """ set up the server """
+    port = get_args(argv)
+    ip_addr = '0.0.0.0'
+    server = MyTCPServer((ip_addr, 1313), MyTCPServerHandler)
+    print "Server established. listening on address: ", ip_addr, " port: ", port 
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main(sys.argv)
